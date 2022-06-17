345f7e2390938d083c156b165c09f60a4cd74b5b
esam014 <samenoch014@gmail.com>
2022-06-17 14:55:38 -0400

    Replaced boiler plate code with hello world program

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Hello World
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello World!</h1>
+        <p>This is a test!</p>
       </header>
     </div>
   );
