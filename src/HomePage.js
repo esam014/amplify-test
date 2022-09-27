894986d5e76dce4c5004c0efae3ada5bf8d03645
esam014 <samenoch014@gmail.com>
2022-09-26 21:06:05 -0400

    added api backend

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,6 +4,16 @@ import Progress from "./Components/HomePage/Progress/Progress";
 import Calendar from "react-calendar";
 import "./Components/HomePage/HomePage.css";
 import Spacer from "./Components/Spacer";
+import { API } from "aws-amplify";
+import * as mutations from './graphql/mutations';
+
+const todoDetails = {
+  name: 'Todo 1',
+  description: 'Learn AWS AppSync'
+};
+
+const newTodo = await API.graphql({ query: mutations.createTodo, variables: {input: todoDetails}});
+
 function HomePage ()
 {
     return (
