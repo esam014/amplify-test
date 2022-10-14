4e8ae3bdacae482f6c8c508a8f8aff9ace0221a1
Timothy Sam <timrsam94@gmail.com>
2022-10-14 02:00:24 +0000

    Add query for short term goal

--- src/Components/HomePage/Goal/Goal.js
+++ src/Components/HomePage/Goal/Goal.js
@@ -1,10 +1,13 @@
 import React from "react";
 import BoxTracker from "../BoxTracker/BoxTracker";
+import { getGoal } from "../../../graphql/queries";
 
 function Goal ()
 {
+
+    const { data } = getGoal("904dba04-1870-4dbf-a3de-eea7e95ac8b3")
     return <div>
-        <BoxTracker Type="Goal" Date="Target Date: mm/dd/yyyy" Weight="##" Waiste="##" Extra="___" />
+        <BoxTracker Type="Goal" Date="Target Date: mm/dd/yyyy" ShortTermGoal={data.short_term_goal} Waist="##" Extra="___" />
     </div>
 }
 
