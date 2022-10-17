53ef7124c7ac910d500b6d38fb9e992392dd55cc
Timothy Sam <timrsam94@gmail.com>
2022-10-17 00:38:10 +0000

    Strff

--- src/Components/HomePage/Goal/Goal.js
+++ src/Components/HomePage/Goal/Goal.js
@@ -3,10 +3,10 @@ import BoxTracker from "../BoxTracker/BoxTracker";
 import { API } from "aws-amplify";
 import { getGoal, listGoals } from "../../../graphql/queries";
 
-function Goal ()
+async function Goal ()
 {
 
-    const apiData = API.graphql({query: listGoals});
+    const apiData =  await API.graphql({query: listGoals});
     const goalData = apiData.data.listGoals.items;
 
     return <div>
