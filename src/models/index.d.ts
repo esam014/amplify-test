f936fed6ef9a01b3f5fee1b427b769604b5f4864
Timothy Sam <timrsam94@gmail.com>
2022-11-03 02:17:29 +0000

    Backend config

--- src/models/index.d.ts
+++ src/models/index.d.ts
@@ -1,4 +1,6 @@
 import { ModelInit, MutableModel } from "@aws-amplify/datastore";
+// @ts-ignore
+import { LazyLoading, LazyLoadingDisabled, AsyncCollection, AsyncItem } from "@aws-amplify/datastore";
 
 export enum ServiceTier {
   NUTRITION = "NUTRITION",
@@ -71,7 +73,7 @@ type ReportMeasurementMetaData = {
   readOnlyFields: 'createdAt' | 'updatedAt';
 }
 
-export declare class Report {
+type EagerReport = {
   readonly id: string;
   readonly start_date?: string | null;
   readonly end_date?: string | null;
@@ -80,11 +82,26 @@ export declare class Report {
   readonly Measurements?: (ReportMeasurement | null)[] | null;
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
-  constructor(init: ModelInit<Report, ReportMetaData>);
-  static copyOf(source: Report, mutator: (draft: MutableModel<Report, ReportMetaData>) => MutableModel<Report, ReportMetaData> | void): Report;
 }
 
-export declare class DailyNutrition {
+type LazyReport = {
+  readonly id: string;
+  readonly start_date?: string | null;
+  readonly end_date?: string | null;
+  readonly clientID: string;
+  readonly DailyNutritions: AsyncCollection<ReportDailyNutrition>;
+  readonly Measurements: AsyncCollection<ReportMeasurement>;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+export declare type Report = LazyLoading extends LazyLoadingDisabled ? EagerReport : LazyReport
+
+export declare const Report: (new (init: ModelInit<Report, ReportMetaData>) => Report) & {
+  copyOf(source: Report, mutator: (draft: MutableModel<Report, ReportMetaData>) => MutableModel<Report, ReportMetaData> | void): Report;
+}
+
+type EagerDailyNutrition = {
   readonly id: string;
   readonly date?: string | null;
   readonly calories?: number | null;
@@ -95,11 +112,28 @@ export declare class DailyNutrition {
   readonly reports?: (ReportDailyNutrition | null)[] | null;
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
-  constructor(init: ModelInit<DailyNutrition, DailyNutritionMetaData>);
-  static copyOf(source: DailyNutrition, mutator: (draft: MutableModel<DailyNutrition, DailyNutritionMetaData>) => MutableModel<DailyNutrition, DailyNutritionMetaData> | void): DailyNutrition;
 }
 
-export declare class Measurement {
+type LazyDailyNutrition = {
+  readonly id: string;
+  readonly date?: string | null;
+  readonly calories?: number | null;
+  readonly proteins?: number | null;
+  readonly carbs?: number | null;
+  readonly fats?: number | null;
+  readonly clientID: string;
+  readonly reports: AsyncCollection<ReportDailyNutrition>;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+export declare type DailyNutrition = LazyLoading extends LazyLoadingDisabled ? EagerDailyNutrition : LazyDailyNutrition
+
+export declare const DailyNutrition: (new (init: ModelInit<DailyNutrition, DailyNutritionMetaData>) => DailyNutrition) & {
+  copyOf(source: DailyNutrition, mutator: (draft: MutableModel<DailyNutrition, DailyNutritionMetaData>) => MutableModel<DailyNutrition, DailyNutritionMetaData> | void): DailyNutrition;
+}
+
+type EagerMeasurement = {
   readonly id: string;
   readonly date?: string | null;
   readonly right_arm?: number | null;
@@ -113,22 +147,55 @@ export declare class Measurement {
   readonly reports?: (ReportMeasurement | null)[] | null;
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
-  constructor(init: ModelInit<Measurement, MeasurementMetaData>);
-  static copyOf(source: Measurement, mutator: (draft: MutableModel<Measurement, MeasurementMetaData>) => MutableModel<Measurement, MeasurementMetaData> | void): Measurement;
 }
 
-export declare class Exercises {
+type LazyMeasurement = {
+  readonly id: string;
+  readonly date?: string | null;
+  readonly right_arm?: number | null;
+  readonly waist?: number | null;
+  readonly hips?: number | null;
+  readonly right_thigh?: number | null;
+  readonly chest?: number | null;
+  readonly weight?: number | null;
+  readonly picture?: string | null;
+  readonly clientID: string;
+  readonly reports: AsyncCollection<ReportMeasurement>;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+export declare type Measurement = LazyLoading extends LazyLoadingDisabled ? EagerMeasurement : LazyMeasurement
+
+export declare const Measurement: (new (init: ModelInit<Measurement, MeasurementMetaData>) => Measurement) & {
+  copyOf(source: Measurement, mutator: (draft: MutableModel<Measurement, MeasurementMetaData>) => MutableModel<Measurement, MeasurementMetaData> | void): Measurement;
+}
+
+type EagerExercises = {
   readonly id: string;
   readonly name?: string | null;
   readonly video?: string | null;
   readonly WodExercises?: (WodExercises | null)[] | null;
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
-  constructor(init: ModelInit<Exercises, ExercisesMetaData>);
-  static copyOf(source: Exercises, mutator: (draft: MutableModel<Exercises, ExercisesMetaData>) => MutableModel<Exercises, ExercisesMetaData> | void): Exercises;
 }
 
-export declare class WodExercises {
+type LazyExercises = {
+  readonly id: string;
+  readonly name?: string | null;
+  readonly video?: string | null;
+  readonly WodExercises: AsyncCollection<WodExercises>;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+export declare type Exercises = LazyLoading extends LazyLoadingDisabled ? EagerExercises : LazyExercises
+
+export declare const Exercises: (new (init: ModelInit<Exercises, ExercisesMetaData>) => Exercises) & {
+  copyOf(source: Exercises, mutator: (draft: MutableModel<Exercises, ExercisesMetaData>) => MutableModel<Exercises, ExercisesMetaData> | void): Exercises;
+}
+
+type EagerWodExercises = {
   readonly id: string;
   readonly resistance?: number | null;
   readonly reps?: number | null;
@@ -138,11 +205,27 @@ export declare class WodExercises {
   readonly exercisesID: string;
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
-  constructor(init: ModelInit<WodExercises, WodExercisesMetaData>);
-  static copyOf(source: WodExercises, mutator: (draft: MutableModel<WodExercises, WodExercisesMetaData>) => MutableModel<WodExercises, WodExercisesMetaData> | void): WodExercises;
 }
 
-export declare class Workout {
+type LazyWodExercises = {
+  readonly id: string;
+  readonly resistance?: number | null;
+  readonly reps?: number | null;
+  readonly sets?: number | null;
+  readonly rest?: number | null;
+  readonly workoutID: string;
+  readonly exercisesID: string;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+export declare type WodExercises = LazyLoading extends LazyLoadingDisabled ? EagerWodExercises : LazyWodExercises
+
+export declare const WodExercises: (new (init: ModelInit<WodExercises, WodExercisesMetaData>) => WodExercises) & {
+  copyOf(source: WodExercises, mutator: (draft: MutableModel<WodExercises, WodExercisesMetaData>) => MutableModel<WodExercises, WodExercisesMetaData> | void): WodExercises;
+}
+
+type EagerWorkout = {
   readonly id: string;
   readonly warm_up?: string | null;
   readonly cool_down?: string | null;
@@ -151,11 +234,26 @@ export declare class Workout {
   readonly WodExercises?: (WodExercises | null)[] | null;
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
-  constructor(init: ModelInit<Workout, WorkoutMetaData>);
-  static copyOf(source: Workout, mutator: (draft: MutableModel<Workout, WorkoutMetaData>) => MutableModel<Workout, WorkoutMetaData> | void): Workout;
 }
 
-export declare class Schedule {
+type LazyWorkout = {
+  readonly id: string;
+  readonly warm_up?: string | null;
+  readonly cool_down?: string | null;
+  readonly muscle_group?: string | null;
+  readonly Schedules: AsyncCollection<Schedule>;
+  readonly WodExercises: AsyncCollection<WodExercises>;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+export declare type Workout = LazyLoading extends LazyLoadingDisabled ? EagerWorkout : LazyWorkout
+
+export declare const Workout: (new (init: ModelInit<Workout, WorkoutMetaData>) => Workout) & {
+  copyOf(source: Workout, mutator: (draft: MutableModel<Workout, WorkoutMetaData>) => MutableModel<Workout, WorkoutMetaData> | void): Workout;
+}
+
+type EagerSchedule = {
   readonly id: string;
   readonly date?: string | null;
   readonly completed?: boolean | null;
@@ -163,22 +261,49 @@ export declare class Schedule {
   readonly workoutID: string;
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
-  constructor(init: ModelInit<Schedule, ScheduleMetaData>);
-  static copyOf(source: Schedule, mutator: (draft: MutableModel<Schedule, ScheduleMetaData>) => MutableModel<Schedule, ScheduleMetaData> | void): Schedule;
 }
 
-export declare class Goal {
+type LazySchedule = {
+  readonly id: string;
+  readonly date?: string | null;
+  readonly completed?: boolean | null;
+  readonly clientID: string;
+  readonly workoutID: string;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+export declare type Schedule = LazyLoading extends LazyLoadingDisabled ? EagerSchedule : LazySchedule
+
+export declare const Schedule: (new (init: ModelInit<Schedule, ScheduleMetaData>) => Schedule) & {
+  copyOf(source: Schedule, mutator: (draft: MutableModel<Schedule, ScheduleMetaData>) => MutableModel<Schedule, ScheduleMetaData> | void): Schedule;
+}
+
+type EagerGoal = {
   readonly id: string;
   readonly short_term_goal?: string | null;
   readonly long_term_goal?: string | null;
   readonly Clients?: (Client | null)[] | null;
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
-  constructor(init: ModelInit<Goal, GoalMetaData>);
-  static copyOf(source: Goal, mutator: (draft: MutableModel<Goal, GoalMetaData>) => MutableModel<Goal, GoalMetaData> | void): Goal;
 }
 
-export declare class Client {
+type LazyGoal = {
+  readonly id: string;
+  readonly short_term_goal?: string | null;
+  readonly long_term_goal?: string | null;
+  readonly Clients: AsyncCollection<Client>;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+export declare type Goal = LazyLoading extends LazyLoadingDisabled ? EagerGoal : LazyGoal
+
+export declare const Goal: (new (init: ModelInit<Goal, GoalMetaData>) => Goal) & {
+  copyOf(source: Goal, mutator: (draft: MutableModel<Goal, GoalMetaData>) => MutableModel<Goal, GoalMetaData> | void): Goal;
+}
+
+type EagerClient = {
   readonly id: string;
   readonly start_date?: string | null;
   readonly account_expiry_date?: string | null;
@@ -202,11 +327,41 @@ export declare class Client {
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
   readonly clientNutritionPlanId?: string | null;
-  constructor(init: ModelInit<Client, ClientMetaData>);
-  static copyOf(source: Client, mutator: (draft: MutableModel<Client, ClientMetaData>) => MutableModel<Client, ClientMetaData> | void): Client;
 }
 
-export declare class NutritionPlan {
+type LazyClient = {
+  readonly id: string;
+  readonly start_date?: string | null;
+  readonly account_expiry_date?: string | null;
+  readonly onboarded?: boolean | null;
+  readonly assessed?: boolean | null;
+  readonly age?: number | null;
+  readonly gender?: number | null;
+  readonly height?: number | null;
+  readonly training_frequency?: number | null;
+  readonly dietary_restrictions?: string | null;
+  readonly equipment_list?: string | null;
+  readonly other_physical_activities?: string | null;
+  readonly service_tier?: ServiceTier | keyof typeof ServiceTier | null;
+  readonly trainerID: string;
+  readonly goalID: string;
+  readonly Schedules: AsyncCollection<Schedule>;
+  readonly DailyNutritions: AsyncCollection<DailyNutrition>;
+  readonly Measurements: AsyncCollection<Measurement>;
+  readonly Reports: AsyncCollection<Report>;
+  readonly NutritionPlan: AsyncItem<NutritionPlan | undefined>;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+  readonly clientNutritionPlanId?: string | null;
+}
+
+export declare type Client = LazyLoading extends LazyLoadingDisabled ? EagerClient : LazyClient
+
+export declare const Client: (new (init: ModelInit<Client, ClientMetaData>) => Client) & {
+  copyOf(source: Client, mutator: (draft: MutableModel<Client, ClientMetaData>) => MutableModel<Client, ClientMetaData> | void): Client;
+}
+
+type EagerNutritionPlan = {
   readonly id: string;
   readonly calorie_target?: number | null;
   readonly protein_target?: number | null;
@@ -215,20 +370,46 @@ export declare class NutritionPlan {
   readonly end_date?: string | null;
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
-  constructor(init: ModelInit<NutritionPlan, NutritionPlanMetaData>);
-  static copyOf(source: NutritionPlan, mutator: (draft: MutableModel<NutritionPlan, NutritionPlanMetaData>) => MutableModel<NutritionPlan, NutritionPlanMetaData> | void): NutritionPlan;
 }
 
-export declare class Trainer {
+type LazyNutritionPlan = {
+  readonly id: string;
+  readonly calorie_target?: number | null;
+  readonly protein_target?: number | null;
+  readonly carb_target?: number | null;
+  readonly fat_target?: number | null;
+  readonly end_date?: string | null;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+export declare type NutritionPlan = LazyLoading extends LazyLoadingDisabled ? EagerNutritionPlan : LazyNutritionPlan
+
+export declare const NutritionPlan: (new (init: ModelInit<NutritionPlan, NutritionPlanMetaData>) => NutritionPlan) & {
+  copyOf(source: NutritionPlan, mutator: (draft: MutableModel<NutritionPlan, NutritionPlanMetaData>) => MutableModel<NutritionPlan, NutritionPlanMetaData> | void): NutritionPlan;
+}
+
+type EagerTrainer = {
   readonly id: string;
   readonly Clients?: (Client | null)[] | null;
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
-  constructor(init: ModelInit<Trainer, TrainerMetaData>);
-  static copyOf(source: Trainer, mutator: (draft: MutableModel<Trainer, TrainerMetaData>) => MutableModel<Trainer, TrainerMetaData> | void): Trainer;
 }
 
-export declare class User {
+type LazyTrainer = {
+  readonly id: string;
+  readonly Clients: AsyncCollection<Client>;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+export declare type Trainer = LazyLoading extends LazyLoadingDisabled ? EagerTrainer : LazyTrainer
+
+export declare const Trainer: (new (init: ModelInit<Trainer, TrainerMetaData>) => Trainer) & {
+  copyOf(source: Trainer, mutator: (draft: MutableModel<Trainer, TrainerMetaData>) => MutableModel<Trainer, TrainerMetaData> | void): Trainer;
+}
+
+type EagerUser = {
   readonly id: string;
   readonly username?: string | null;
   readonly first_name?: string | null;
@@ -237,39 +418,95 @@ export declare class User {
   readonly email?: string | null;
   readonly Trainer?: Trainer | null;
   readonly Client?: Client | null;
+  readonly sub?: string | null;
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
   readonly userTrainerId?: string | null;
   readonly userClientId?: string | null;
-  constructor(init: ModelInit<User, UserMetaData>);
-  static copyOf(source: User, mutator: (draft: MutableModel<User, UserMetaData>) => MutableModel<User, UserMetaData> | void): User;
 }
 
-export declare class Todo {
+type LazyUser = {
+  readonly id: string;
+  readonly username?: string | null;
+  readonly first_name?: string | null;
+  readonly last_name?: string | null;
+  readonly role?: Role | keyof typeof Role | null;
+  readonly email?: string | null;
+  readonly Trainer: AsyncItem<Trainer | undefined>;
+  readonly Client: AsyncItem<Client | undefined>;
+  readonly sub?: string | null;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+  readonly userTrainerId?: string | null;
+  readonly userClientId?: string | null;
+}
+
+export declare type User = LazyLoading extends LazyLoadingDisabled ? EagerUser : LazyUser
+
+export declare const User: (new (init: ModelInit<User, UserMetaData>) => User) & {
+  copyOf(source: User, mutator: (draft: MutableModel<User, UserMetaData>) => MutableModel<User, UserMetaData> | void): User;
+}
+
+type EagerTodo = {
   readonly id: string;
   readonly description?: string | null;
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
-  constructor(init: ModelInit<Todo, TodoMetaData>);
-  static copyOf(source: Todo, mutator: (draft: MutableModel<Todo, TodoMetaData>) => MutableModel<Todo, TodoMetaData> | void): Todo;
 }
 
-export declare class ReportDailyNutrition {
+type LazyTodo = {
+  readonly id: string;
+  readonly description?: string | null;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+export declare type Todo = LazyLoading extends LazyLoadingDisabled ? EagerTodo : LazyTodo
+
+export declare const Todo: (new (init: ModelInit<Todo, TodoMetaData>) => Todo) & {
+  copyOf(source: Todo, mutator: (draft: MutableModel<Todo, TodoMetaData>) => MutableModel<Todo, TodoMetaData> | void): Todo;
+}
+
+type EagerReportDailyNutrition = {
   readonly id: string;
   readonly report: Report;
   readonly dailyNutrition: DailyNutrition;
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
-  constructor(init: ModelInit<ReportDailyNutrition, ReportDailyNutritionMetaData>);
-  static copyOf(source: ReportDailyNutrition, mutator: (draft: MutableModel<ReportDailyNutrition, ReportDailyNutritionMetaData>) => MutableModel<ReportDailyNutrition, ReportDailyNutritionMetaData> | void): ReportDailyNutrition;
 }
 
-export declare class ReportMeasurement {
+type LazyReportDailyNutrition = {
+  readonly id: string;
+  readonly report: AsyncItem<Report>;
+  readonly dailyNutrition: AsyncItem<DailyNutrition>;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+export declare type ReportDailyNutrition = LazyLoading extends LazyLoadingDisabled ? EagerReportDailyNutrition : LazyReportDailyNutrition
+
+export declare const ReportDailyNutrition: (new (init: ModelInit<ReportDailyNutrition, ReportDailyNutritionMetaData>) => ReportDailyNutrition) & {
+  copyOf(source: ReportDailyNutrition, mutator: (draft: MutableModel<ReportDailyNutrition, ReportDailyNutritionMetaData>) => MutableModel<ReportDailyNutrition, ReportDailyNutritionMetaData> | void): ReportDailyNutrition;
+}
+
+type EagerReportMeasurement = {
   readonly id: string;
   readonly report: Report;
   readonly measurement: Measurement;
   readonly createdAt?: string | null;
   readonly updatedAt?: string | null;
-  constructor(init: ModelInit<ReportMeasurement, ReportMeasurementMetaData>);
-  static copyOf(source: ReportMeasurement, mutator: (draft: MutableModel<ReportMeasurement, ReportMeasurementMetaData>) => MutableModel<ReportMeasurement, ReportMeasurementMetaData> | void): ReportMeasurement;
+}
+
+type LazyReportMeasurement = {
+  readonly id: string;
+  readonly report: AsyncItem<Report>;
+  readonly measurement: AsyncItem<Measurement>;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+export declare type ReportMeasurement = LazyLoading extends LazyLoadingDisabled ? EagerReportMeasurement : LazyReportMeasurement
+
+export declare const ReportMeasurement: (new (init: ModelInit<ReportMeasurement, ReportMeasurementMetaData>) => ReportMeasurement) & {
+  copyOf(source: ReportMeasurement, mutator: (draft: MutableModel<ReportMeasurement, ReportMeasurementMetaData>) => MutableModel<ReportMeasurement, ReportMeasurementMetaData> | void): ReportMeasurement;
 }
