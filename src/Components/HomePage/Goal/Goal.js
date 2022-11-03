bf3d81e68605d0a9d60351d435aa0824c72e53b1
Timothy Sam <timrsam94@gmail.com>
2022-11-03 02:59:30 +0000

    Handle not-yet-created goals

--- src/Components/HomePage/Goal/Goal.js
+++ src/Components/HomePage/Goal/Goal.js
@@ -6,7 +6,7 @@ import { getClient, getGoal, listUsers } from "../../../graphql/queries";
 
 function Goal ()
 {
-    const [datExport, setData ] = useState([]);
+    const [shortExport, setData ] = useState([]);
     const [anotherData, setAnother] = useState([]);
     useEffect(() => {
         retrieveGoals();
@@ -21,12 +21,24 @@ function Goal ()
             const clientID = userID.data.listUsers.items[0].userClientId;
             const clientData = await API.graphql(graphqlOperation(getClient, {id: clientID}));
             const goalID = clientData.data.getClient.goalID;
-            const apiData =  await API.graphql(graphqlOperation(getGoal, {id: goalID}));
-            const datExport = apiData.data.getGoal.short_term_goal;
-            const longExport = apiData.data.getGoal.long_term_goal;
-            setData(datExport);
-            setAnother(longExport);
-            console.info(longExport); 
+            if (goalID != null) {
+                const apiData =  await API.graphql(graphqlOperation(getGoal, {id: goalID}));
+                const shortExport = apiData.data.getGoal.short_term_goal;
+                const longExport = apiData.data.getGoal.long_term_goal;
+                setData(shortExport);
+                setAnother(longExport);
+                console.info(shortExport);
+                console.info(longExport);
+            }
+            else {
+                const shortExport = "Your trainer is working on developing your short-term goal still...";
+                const longExport = "Your trainer is working on developing your long-term goals still...";
+                setData(shortExport);
+                setAnother(longExport);
+                console.info(shortExport);
+                console.info(longExport);
+            }
+            
         } catch (error) {
             console.error("Failure retrieving goals!", error)
         }
@@ -34,7 +46,7 @@ function Goal ()
 
 
     return (<div>
-        <BoxTracker Type="Goal" Date="Target Date: mm/dd/yyyy" ShortTermGoal={datExport} LongTermGoal={anotherData}/>
+        <BoxTracker Type="Goal" Date="Target Date: mm/dd/yyyy" ShortTermGoal={shortExport} LongTermGoal={anotherData}/>
     </div>)
 }
 export default Goal
