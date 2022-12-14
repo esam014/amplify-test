35f5a03cffd98c381436a17bd9fcb0ea870be8fe
Timothy Sam <timrsam94@gmail.com>
2022-12-14 05:35:47 +0000

    Data updates complete

--- src/Components/TrainerApp/Registration/MultiStepForm.js
+++ src/Components/TrainerApp/Registration/MultiStepForm.js
@@ -4,9 +4,9 @@ import UserDetails from "./UserDetails";
 import Progress from './Progress';
 import AccountDetails from './AccountDetails';
 import PlatformDetails from './PlatformDetails';
-import { schema } from '../../../models/schema';
 import { API, graphqlOperation } from "aws-amplify";
-import { createPlatformConfig } from '../../../graphql/mutations';
+import { updatePlatformConfig, updateUser } from '../../../graphql/mutations';
+import { schema } from '../../../models/schema';
 
 class MultiStepForm extends Component {
     constructor(props) {
@@ -16,23 +16,23 @@ class MultiStepForm extends Component {
             user: this.props.user,
             platformConfig: this.props.platformConfig,
             // User Details
-            firstName: '',
-            lastName: '',
-            email: '',
-            phone: '',
-            address: '',
-            city: '',
-            usstate: '',
-            zip: '',
+            firstName: this.props.user.first_name,
+            lastName: this.props.user.last_name,
+            email: this.props.user.email,
+            phone: this.props.user.phone,
+            address: this.props.user.address,
+            city: this.props.user.city,
+            usstate: this.props.user.usstate,
+            zip: this.props.user.zip,
             // Platform details
-            logoFile: '',
-            faviconFile: '',
-            primaryColor: '#254AA2',
-            secondaryColor: '#E19614',
-            customUrl: '',
-            persWebEnabled: false,
-            platformNutrition: false,
-            platformTrainer: false,
+            logoFile: this.props.platformConfig.logo_file,
+            faviconFile: this.props.platformConfig.favicon_file,
+            primaryColor: this.props.platformConfig.primary_color,
+            secondaryColor: this.props.platformConfig.secondary_color,
+            customUrl: this.props.platformConfig.custom_url,
+            persWebEnabled: this.props.platformConfig.personal_web_enabled,
+            platformNutrition: this.props.platformConfig.platform_nutrition_enabled,
+            platformTrainer: this.props.platformConfig.platform_trainer_enabled,
         }
     }
 
@@ -54,6 +54,62 @@ class MultiStepForm extends Component {
         })
     }
 
+    commitUser = async () => {
+
+        var update = {
+            id: this.state.user.id,
+            first_name: this.state.firstName,
+            last_name: this.state.lastName,
+            phone: this.state.phone,
+            address: this.state.address,
+            city: this.state.city,
+            usstate: this.state.usstate,
+            zip: this.state.zip,
+        }
+        try {
+            var resp = await API.graphql(graphqlOperation(updateUser, { input: update }));
+            this.setState({ user: resp.data.updateUser });
+        } catch (error){
+            console.error("Failed to update user:", error);
+        }
+    }
+
+    finalCommit = async () => {
+        var update = {
+            id: this.state.user.id,
+            lifecycle: schema.enums.Lifecycle.values[3] //Active Status
+        }
+        try {
+            var resp = await API.graphql(graphqlOperation(updateUser, { input: update }));
+            this.setState({ user: resp.data.updateUser });
+        } catch (error){
+            console.error("Failed to update user:", error);
+        }
+    }
+
+    commitPlatform = async () => {
+
+        var update = {
+            id: this.state.platformConfig.id,
+            logo_file: this.state.logoFile,
+            favicon_file: this.state.faviconFile,
+            primary_color: this.state.primaryColor,
+            secondary_color: this.state.secondaryColor,
+            custom_url: this.state.customUrl,
+            personal_web_enabled: this.state.persWebEnabled,
+            platform_nutrition_enabled: this.state.platformNutrition,
+            platform_trainer_enabled: this.state.platformTrainer,
+        }
+        try {
+            console.info(update);
+            var resp = await API.graphql(graphqlOperation(updatePlatformConfig, { input: update }));
+            this.setState({ platformConfig: resp.data.updatePlatformConfig });
+        } catch (error){
+            console.error("Failed to update platform configurations:", error);
+        }
+    }
+
+
     handleChange = (event) => {
 
         if (event.target.type === "checkbox") {
@@ -65,10 +121,9 @@ class MultiStepForm extends Component {
     }
 
 
-
     render() {
         const { step, user, platformConfig, firstName, lastName, email, phone, address, city, usstate, zip, logoFile, faviconFile, primaryColor, secondaryColor, customUrl, persWebEnabled, platformNutrition, platformTrainer } = this.state;
-        const inputValues = {user, platformConfig, firstName, lastName, email, phone, address, city, usstate, zip, logoFile, faviconFile, primaryColor, secondaryColor, customUrl, persWebEnabled, platformNutrition, platformTrainer };
+        const inputValues = { user, platformConfig, firstName, lastName, email, phone, address, city, usstate, zip, logoFile, faviconFile, primaryColor, secondaryColor, customUrl, persWebEnabled, platformNutrition, platformTrainer };
         <Progress />
         switch (step) {
             case 1:
@@ -79,6 +134,7 @@ class MultiStepForm extends Component {
                             nextStep={this.nextStep}
                             handleChange={this.handleChange}
                             inputValues={inputValues}
+                            commit={this.commitUser}
                         />
                     </div>
                 ]
@@ -102,8 +158,16 @@ class MultiStepForm extends Component {
                             prevStep={this.prevStep}
                             handleChange={this.handleChange}
                             inputValues={inputValues}
+                            commit={this.commitPlatform}
                         />
                     </div>]
+            case 4:
+                this.finalCommit();
+                return [
+                    <div key="something made up">
+                        <p>Congrats, you've finished registration. Need to return to home page.</p>
+                    </div>
+                ]
         }
     }
 }
